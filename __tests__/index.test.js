bf3b8f54ba2178e66fc24b8aa47116fc42b5029e
Artem Arefiev <a.arefiev@inbox.ru>
2018-02-08 17:40:48 +0400

    Refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,17 @@
+import 'js-polyfills/dom';
+import 'js-polyfills/url';
+import 'js-polyfills/html';
+
 import fs from 'fs';
 import path from 'path';
-import moxios from 'moxios';
+import timer from 'timer-promise';
+import nock from 'nock';
 import keycode from 'keycode';
 import { html } from 'js-beautify';
 
 import run from '../src';
 
+const host = 'http://localhost';
 const fixturesPath = path.join(__dirname, '__fixtures__');
 const getTree = () => html(document.body.innerHTML);
 
@@ -16,25 +22,44 @@ const pressKey = (key, el = document.body, value = key) => {
   el.dispatchEvent(e);
 };
 
+const submitForm = (el = document.body) => {
+  const e = new Event('submit');
+  el.dispatchEvent(e);
+};
+
 beforeAll(async () => {
+  jsdom.reconfigure({ url: host }); // eslint-disable-line
+
   const initHtml = fs.readFileSync(path.join(fixturesPath, 'index.html')).toString();
 
   document.documentElement.innerHTML = initHtml;
-  moxios.install();
+  nock.disableNetConnect();
   run();
 });
 
-test('application', async (done) => {
+test('application', async () => {
   const input = document.getElementById('rss_input_url');
+  const form = document.getElementById('rss_form');
   const feedURL = 'http://lorem-rss.herokuapp.com/feed';
+  const feedInXML = fs.readFileSync(path.join(fixturesPath, 'feed.xml'), 'utf8')
+    .toString();
 
   expect(getTree()).toMatchSnapshot();
 
   pressKey('h', input);
+  await timer.start(100);
   expect(getTree()).toMatchSnapshot();
 
   pressKey(feedURL, input);
+  await timer.start(100);
   expect(getTree()).toMatchSnapshot();
 
-  done();
+  nock(host)
+    .get('/api/rss')
+    .query({ url: feedURL })
+    .reply(200, feedInXML);
+
+  submitForm(form);
+  await timer.start(100);
+  expect(getTree()).toMatchSnapshot();
 });
